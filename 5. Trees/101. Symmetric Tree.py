e539133fda608f0e9a2fa2d6324bbb52c98b7891
Mike Cheung <56772737+FireZebra@users.noreply.github.com>
2020-07-22 16:23:26 -0700

    Update 101. Symmetric Tree.py

--- 5. Trees/101. Symmetric Tree.py
+++ 5. Trees/101. Symmetric Tree.py
@@ -1,32 +1,54 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+"""
+recursive
+remarks: Start from root of tree and touch all the children downwards, checking the mirror condition.
+time: O(N), N = # of nodes
+space: O(h), h = height of the tree
 
-# recrusive 
-# time: O(N)
-# space: O(N)
-class Solution(object):
-    def isSymmetric(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        if not root:
+iterative with stack
+remarks: Same idea as recursive sol.
+time: O(N), N = # of nodes
+space: O(N)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def check(root1, root2):
+            if root1 is None and root2 is None:
+                return True
+            if root1 is None or root2 is None:
+                return False
+            if root1.val != root2.val:
+                return False
+            return check(root1.left, root2.right) and check(root1.right, root2.left)
+        
+        if root is None:
             return True
         
-        return self.isMirror(root.left, root.right)
-        
-    def isMirror(self, left, right):
-        if not left and not right:
+        return check(root.left, root.right)
+    
+    
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None: 
             return True
         
-        if not left or not right:
-            return False
+        stack = [(root.left, root.right)]
         
-        if left.val != right.val:
-            return False
-        
-        return self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
+        while stack:
+            left, right = stack.pop(0)
+            if left is None and right is None:
+                continue
+            if left is None or right is None:
+                return False
+            if left.val != right.val:
+                return False
+            stack.append((left.left, right.right))
+            stack.append((left.right, right.left))
+            
+        return True
